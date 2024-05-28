e74a33d8b66e934c1e5d290dc5db9413f89b7ac4
knqyf263 <knqyf263@gmail.com>
2024-05-28 16:17:17 +0400

    feat: parse GitHub URL
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- pkg/crawl/git/url.go
+++ pkg/crawl/git/url.go
@@ -3,6 +3,7 @@ package git
 import (
 	"fmt"
 	"net/url"
+	"path"
 	"strings"
 )
 
@@ -16,8 +17,17 @@ func NormalizeURL(rawurl string) (*url.URL, error) {
 	// Force the git protocol
 	u.Scheme = "git::https"
 
+	// GitHub specific
+	if u.Host == "github.com" {
+		normalizeGitHubURL(u)
+	}
+
+	// TODO: GitLab specific
+	if u.Host == "gitlab.com" {
+	}
+
 	// Add .git suffix
-	if !strings.HasSuffix(u.Path, ".git") {
+	if !strings.HasSuffix(u.Path, ".git") && !strings.Contains(u.Path, "//") {
 		u.Path += ".git"
 	}
 
@@ -27,4 +37,31 @@ func NormalizeURL(rawurl string) (*url.URL, error) {
 	u.RawQuery = q.Encode()
 
 	return u, nil
+}
+
+func normalizeGitHubURL(u *url.URL) {
+	// Split the path
+	parts := strings.Split(u.Path, "/")
+
+	// e.g. /<owner>/<repo>/tree/<ref>/<subpath1>/<subpath2>
+	if len(parts) <= 5 {
+		return
+	}
+
+	if parts[3] == "tree" {
+		ref := parts[4]
+
+		// Add ref
+		q := u.Query()
+		q.Add("ref", ref)
+		u.RawQuery = q.Encode()
+	}
+
+	if len(parts) <= 6 {
+		return
+	}
+
+	// Add subdirectories
+	// cf. https://github.com/hashicorp/go-getter?tab=readme-ov-file#subdirectories
+	u.Path = path.Join(parts[1], parts[2]) + ".git//" + path.Join(parts[5:]...)
 }
