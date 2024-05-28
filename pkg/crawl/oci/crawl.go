4c1527c3588052e8bda480368790570f4e3707cf
knqyf263 <knqyf263@gmail.com>
2024-05-28 16:17:00 +0400

    feat(oci): add support for annotations
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- pkg/crawl/oci/crawl.go
+++ pkg/crawl/oci/crawl.go
@@ -7,12 +7,13 @@ import (
 	"github.com/aquasecurity/vex-collector/pkg/crawl/vex"
 	"github.com/aquasecurity/vex-collector/pkg/vexhub"
 	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/package-url/packageurl-go"
 	"log/slog"
 )
 
-const imageSourceLabel = "org.opencontainers.image.source"
+const imageSourceAnnotation = "org.opencontainers.image.source"
 
 type Crawler struct {
 	rootDir string
@@ -59,17 +60,10 @@ func (c *Crawler) detectSrc(purl packageurl.PackageURL) (string, error) {
 		return "", fmt.Errorf("reading image %q: %v", refStr, err)
 	}
 
-	cfg, err := img.ConfigFile()
+	src, err := c.findImageSource(refStr, img)
 	if err != nil {
-		return "", fmt.Errorf("reading config %q: %v", refStr, err)
+		return "", fmt.Errorf("finding image source: %w", err)
 	}
-
-	src, ok := cfg.Config.Labels[imageSourceLabel]
-	if !ok {
-		return "", fmt.Errorf("%s not found in %s", imageSourceLabel, refStr)
-	}
-	slog.Info("Found an image label", slog.String("label", imageSourceLabel),
-		slog.String("value", src))
 
 	u, err := git.NormalizeURL(src)
 	if err != nil {
@@ -77,4 +71,34 @@ func (c *Crawler) detectSrc(purl packageurl.PackageURL) (string, error) {
 	}
 
 	return u.String(), nil
+}
+
+func (c *Crawler) findImageSource(ref string, img v1.Image) (string, error) {
+	// First, try labels in config
+	cfg, err := img.ConfigFile()
+	if err != nil {
+		return "", fmt.Errorf("reading config %q: %v", ref, err)
+	}
+
+	src, ok := cfg.Config.Labels[imageSourceAnnotation]
+	if ok {
+		slog.Info("Found an image label", slog.String("label", imageSourceAnnotation),
+			slog.String("value", src))
+		return src, nil
+	}
+
+	// Next, try annotations in manifest
+	m, err := img.Manifest()
+	if err != nil {
+		return "", fmt.Errorf("reading manifest %q: %v", ref, err)
+	}
+
+	src, ok = m.Annotations[imageSourceAnnotation]
+	if ok {
+		slog.Info("Found an image annotation", slog.String("annotation", imageSourceAnnotation),
+			slog.String("value", src))
+		return src, nil
+	}
+
+	return "", fmt.Errorf("%s not found in %s", imageSourceAnnotation, ref)
 }
