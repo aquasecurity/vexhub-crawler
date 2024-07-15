b2a67ae78d2c5158974264785301ea85bb77da1a
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2024-07-15 12:23:35 +0600

    test(cargo): add unit tests (#15)

--- pkg/crawl/cargo/crawl.go
+++ pkg/crawl/cargo/crawl.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/aquasecurity/vexhub-crawler/pkg/config"
 	"github.com/aquasecurity/vexhub-crawler/pkg/crawl/git"
@@ -18,14 +19,34 @@ type Response struct {
 	} `json:"crate"`
 }
 
-type Crawler struct{}
-
-func NewCrawler() *Crawler {
-	return &Crawler{}
+type Crawler struct {
+	url string
 }
 
+type Option func(*Crawler)
+
+func WithURL(url string) Option {
+	return func(c *Crawler) {
+		c.url = url
+	}
+}
+
+func NewCrawler(opts ...Option) *Crawler {
+	crawler := &Crawler{
+		url: cratesAPI,
+	}
+	for _, opt := range opts {
+		opt(crawler)
+	}
+	return crawler
+}
 func (c *Crawler) DetectSrc(ctx context.Context, pkg config.Package) (string, error) {
-	rawurl := cratesAPI + pkg.PURL.Name
+	// Cargo doesn't use `namespace`
+	// cf. https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#cargo
+	rawurl, err := url.JoinPath(c.url, pkg.PURL.Name)
+	if err != nil {
+		return "", fmt.Errorf("failed to build url: %w", err)
+	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, rawurl, nil)
 	if err != nil {
@@ -42,6 +63,9 @@ func (c *Crawler) DetectSrc(ctx context.Context, pkg config.Package) (string, er
 		return "", fmt.Errorf("failed to get package info: %w", err)
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get package info: %s", resp.Status)
+	}
 
 	var r Response
 	if err = json.NewDecoder(resp.Body).Decode(&r); err != nil {
